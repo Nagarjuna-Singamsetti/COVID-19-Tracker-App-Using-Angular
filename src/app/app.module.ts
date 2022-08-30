8396769798dd6a508b2df25dca21b682ec555f67
Nagarjuna-Singamsetti <naga0955@gmail.com>
2022-08-30 20:20:55 +0530

    Initial Structure Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StateWiseComponent } from './components/state-wise/state-wise.component';
+import { DistrictWiseComponent } from './components/district-wise/district-wise.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StateWiseComponent,
+    DistrictWiseComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/district-wise/district-wise.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-district-wise',
+  templateUrl: './district-wise.component.html',
+  styleUrls: ['./district-wise.component.css']
+})
+export class DistrictWiseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
